47856ef6d0456bbc263c7a097bfeb57fd67b87c6
LTX <qyvjc1@gmail.com>
2023-10-25 20:02:06 +0300

    update encrypt, decrypt, generate dto

--- src/main/java/com/example/rsalab/dto/rsa/generate/GenerateKeyRespondDto.java
+++ src/main/java/com/example/rsalab/dto/rsa/generate/GenerateKeyRespondDto.java
@@ -1,4 +1,9 @@
 package com.example.rsalab.dto.rsa.generate;
 
+import lombok.Data;
+
+@Data
 public class GenerateKeyRespondDto {
+    private String modulus;
+    private String publicExponent;
 }
