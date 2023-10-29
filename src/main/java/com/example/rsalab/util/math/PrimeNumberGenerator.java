21c1b1cd3405f7f39d205e466be27826dba1bbdf
Kostia <47629852+kosssst@users.noreply.github.com>
2023-10-29 09:38:47 +0200

    added prime number generator

--- src/main/java/com/example/rsalab/util/math/PrimeNumberGenerator.java
+++ src/main/java/com/example/rsalab/util/math/PrimeNumberGenerator.java
@@ -2,12 +2,29 @@ package com.example.rsalab.util.math;
 
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
 import java.util.function.Supplier;
 
 @Component
 public class PrimeNumberGenerator implements Supplier<Long> {
     @Override
     public Long get() {
-        return null;
+        SecureRandom secureRandom = new SecureRandom();
+        long candidate = secureRandom.nextLong();
+        while (!checkIfPrime(candidate)) {
+            candidate = secureRandom.nextLong();
+        }
+        return candidate;
+    }
+
+    private boolean checkIfPrime(Long number) {
+        boolean result = true;
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
