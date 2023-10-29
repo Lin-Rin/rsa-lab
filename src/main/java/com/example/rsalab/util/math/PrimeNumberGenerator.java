2a8fe5136735ebd7a17fd300d2762cb587c44a85
LTX <qyvjc1@gmail.com>
2023-10-30 00:51:37 +0200

    refactor

--- src/main/java/com/example/rsalab/util/math/PrimeNumberGenerator.java
+++ src/main/java/com/example/rsalab/util/math/PrimeNumberGenerator.java
@@ -21,17 +21,17 @@ public class PrimeNumberGenerator implements Supplier<BigInteger> {
     @Override
     public BigInteger get() {
         Predicate<BigInteger> simplicityTest = new SimplicityTest();
-        BigInteger number = new BigInteger(generator.generate(this.length), 2);
-        BigInteger limit = BigInteger.TWO.pow(this.length).subtract(number).divide(BigInteger.TWO);
-
+        BigInteger number = new BigInteger("1" + generator.generate(this.length - 1), 2);
         if (number.mod(BigInteger.TWO).equals(BigInteger.ZERO)) number = number.add(BigInteger.ONE);
+
+        BigInteger limit = BigInteger.TWO.pow(this.length).subtract(number).divide(BigInteger.TWO);
 
         while (true) {
             for (BigInteger i = BigInteger.ZERO; i.compareTo(limit) <= 0; i = i.add(BigInteger.ONE)) {
                 BigInteger newNumber = number.add(i.multiply(BigInteger.TWO));
                 if (simplicityTest.test(newNumber)) return newNumber;
             }
-            number = new BigInteger(generator.generate(this.length), 2);
+            number = new BigInteger("1" + generator.generate(this.length - 1), 2);
         }
     }
 }
