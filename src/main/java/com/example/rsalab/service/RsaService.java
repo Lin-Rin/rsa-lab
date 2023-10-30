e9a175aa03607308e72aa5417decef328fe2dab3
LTX <qyvjc1@gmail.com>
2023-10-30 19:20:34 +0200

    renaming params + refactor generateKeys

--- src/main/java/com/example/rsalab/service/RsaService.java
+++ src/main/java/com/example/rsalab/service/RsaService.java
@@ -1,5 +1,6 @@
 package com.example.rsalab.service;
 
+import com.example.rsalab.model.PrivateKey;
 import com.example.rsalab.util.math.PrimeNumberGenerator;
 import org.springframework.stereotype.Component;
 import java.math.BigInteger;
@@ -14,33 +15,17 @@ public class RsaService {
 
     public List<BigInteger> generateKeys(int length) {
         List<BigInteger> list = new ArrayList<>();
-
         supplier.setLength(length);
 
-        for (;;) {
-            list.clear();
-
-            list.add(supplier.get());
-            list.add(supplier.get());
-            list.add(supplier.get());
-            list.add(supplier.get());
-
-            list = list.stream()
-                    .sorted(Comparator.reverseOrder())
-                    .collect(Collectors.toList());
-
-            BigInteger p = new BigInteger(list.get(0).toString());
-            BigInteger q = new BigInteger(list.get(1).toString());
-            BigInteger p1 = new BigInteger(list.get(2).toString());
-            BigInteger q1 = new BigInteger(list.get(3).toString());
-
-            if ((p.multiply(q)).compareTo((p1.multiply(q1))) > 0) {
-                break;
-            }
-        }
+        list.add(supplier.get());
+        list.add(supplier.get());
+        list.add(supplier.get());
+        list.add(supplier.get());
 
         // -> 3, 2, 1
-        return list;
+        return list.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public BigInteger getD(BigInteger p, BigInteger q, BigInteger exponent) {
@@ -49,27 +34,46 @@ public class RsaService {
         return exponent.modInverse(phi);
     }
 
-    public BigInteger encrypt(BigInteger message, BigInteger n, BigInteger exponent) {
-        return message.modPow(exponent, n);
+    public BigInteger getK(BigInteger n, int length) {
+        supplier.setLength(length);
+        BigInteger k = supplier.get();
+
+        while (k.compareTo(n) > 0) {
+            k = supplier.get();
+        }
+
+        return k;
     }
 
-    public BigInteger decrypt(BigInteger cipherText, BigInteger d, BigInteger n) {
-        return cipherText.modPow(d, n);
+    public BigInteger encrypt(BigInteger message, BigInteger modulus, BigInteger exponent) {
+        return message.modPow(exponent, modulus);
     }
 
-    public BigInteger sign(BigInteger message, BigInteger d, BigInteger n) {
-        return message.modPow(d, n);
+    public BigInteger decrypt(BigInteger cipherText, BigInteger d, BigInteger innerModulus) {
+        return cipherText.modPow(d, innerModulus);
     }
 
-    public Boolean verify(BigInteger message, BigInteger signature, BigInteger n, BigInteger exponent) {
-        return signature.modPow(exponent, n).equals(message);
+    public BigInteger sign(BigInteger message, BigInteger d, BigInteger innerModulus) {
+        return message.modPow(d, innerModulus);
     }
 
-    public BigInteger sendKey() {
-        return null;
+    public Boolean verify(BigInteger message, BigInteger signature, BigInteger modulus, BigInteger exponent) {
+        return signature.modPow(exponent, modulus).equals(message);
     }
 
-    public BigInteger receiveKey() {
-        return null;
+    public BigInteger sendKey(BigInteger number, BigInteger modulus, BigInteger exponent) {
+        return number.modPow(exponent, modulus);
+    }
+
+    public BigInteger sendSignature(BigInteger k, PrivateKey privateKey) {
+        return k.modPow(privateKey.getD(), privateKey.getN());
+    }
+
+    public BigInteger receiveConfiguration(BigInteger number, BigInteger d, BigInteger innerModulus) {
+        return number.modPow(d, innerModulus);
+    }
+
+    public BigInteger receiveAuthentication(BigInteger s, BigInteger modulus, BigInteger exponent) {
+        return s.modPow(exponent, modulus);
     }
 }
