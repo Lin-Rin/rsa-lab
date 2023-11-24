408c00e59c15f047f9ac57c08dd5ae6a55f50673
LTX <qyvjc1@gmail.com>
2023-11-24 16:31:17 +0200

    impl encrypt method

--- src/main/java/com/example/rsalab/controller/RabinController.java
+++ src/main/java/com/example/rsalab/controller/RabinController.java
@@ -1,8 +1,11 @@
 package com.example.rsalab.controller;
 
+import com.example.rsalab.dto.rabin.encrypt.EncryptRequest;
+import com.example.rsalab.dto.rabin.encrypt.EncryptResponse;
 import com.example.rsalab.dto.rabin.generate.ServerKeyResponse;
 import com.example.rsalab.service.RabinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,12 @@ public class RabinController {
             @RequestParam("keySize") Long keySize
     ) {
         return rabinService.serverKey(keySize);
+    }
+
+    public EncryptResponse encrypt(
+            @RequestBody EncryptRequest request
+    ) {
+        return rabinService.encrypt(request);
     }
 
 }
--- src/main/java/com/example/rsalab/service/RabinService.java
+++ src/main/java/com/example/rsalab/service/RabinService.java
@@ -1,16 +1,15 @@
 package com.example.rsalab.service;
 
 import com.example.rsalab.dto.rabin.decrypt.DecryptResponse;
+import com.example.rsalab.dto.rabin.encrypt.EncryptRequest;
 import com.example.rsalab.dto.rabin.encrypt.EncryptResponse;
 import com.example.rsalab.dto.rabin.generate.ServerKeyResponse;
 import com.example.rsalab.model.RabinPrivateKey;
 import com.example.rsalab.util.math.rabin.MathUtil;
 import com.example.rsalab.util.math.rabin.NumberGenerator;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.math.BigInteger;
-import java.util.List;
+import java.util.List;import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
@@ -38,12 +37,41 @@ public class RabinService {
     }
 
     // (y, c1, c2)
-    public EncryptResponse encrypt(BigInteger n, BigInteger x) {
+    public EncryptResponse encrypt(EncryptRequest request) {
         EncryptResponse response = new EncryptResponse();
-        var r = generator.get64BitNumber();
-        var newX = r;
+
+        BigInteger m = new BigInteger(request.getText());
+        BigInteger n = new BigInteger(request.getModulus());
+        BigInteger b = new BigInteger(request.getB());
+
+        var x = formatMessage(m, request.getModulus().length());
+        var temp = x.add(b.multiply(BigInteger.valueOf(2).modInverse(n)));
+
+        var y = x.multiply((x.add(b))).mod(n);
+        var c1 = temp.mod(n).mod(BigInteger.TWO);
+        var c2 = mathUtil.getJacobiSymbol(temp, n);
+
+        if (!c2.equals(BigInteger.ONE)) {
+            c2 = BigInteger.ZERO;
+        }
+
+        response.setCiphertext(y.toString(16));
+        response.setParity(c1.toString(16));
+        response.setJacobiSymbol(c2.toString(16));
 
         return response;
+    }
+
+    private BigInteger formatMessage(BigInteger message, int n) {
+        int l = (int) Math.ceil((double) n / 8);
+
+        BigInteger r = generator.get64BitNumber();
+
+        var x = BigInteger.valueOf(255).multiply(BigInteger.valueOf(2).pow(8 * (l - 2)));
+        x = x.add(message.multiply(BigInteger.valueOf(2).pow(64)));
+        x = x.add(r);
+
+        return x;
     }
 
     // (y, c1, c2)
