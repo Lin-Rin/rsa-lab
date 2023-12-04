940c4f2b4a30d8e3d6662fb7e5d4ae3b6657534a
LTX <qyvjc1@gmail.com>
2023-12-04 19:50:04 +0200

    add impl decrypt

--- src/main/java/com/example/rsalab/service/RabinService.java
+++ src/main/java/com/example/rsalab/service/RabinService.java
@@ -9,10 +9,13 @@ import com.example.rsalab.dto.rabin.sign.SignRequest;
 import com.example.rsalab.dto.rabin.sign.SignResponse;
 import com.example.rsalab.dto.rabin.verify.VerifyRequest;
 import com.example.rsalab.dto.rabin.verify.VerifyResponse;
+import com.example.rsalab.model.RabinPublicKey;
 import com.example.rsalab.model.RabinPrivateKey;
 import com.example.rsalab.util.math.rabin.MathUtil;
 import com.example.rsalab.util.math.rabin.NumberGenerator;
 import java.math.BigInteger;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,12 +23,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabinService {
     private RabinPrivateKey privateKey;
+    private RabinPublicKey publicKey;
     private final MathUtil mathUtil;
     private final NumberGenerator generator;
 
     public ServerKeyResponse serverKey(Long keySize) {
         ServerKeyResponse response = new ServerKeyResponse();
         privateKey = new RabinPrivateKey();
+        publicKey = new RabinPublicKey();
 
         var p = generator.getPrimeNumber(keySize);
         var q = generator.getPrimeNumber(keySize);
@@ -34,9 +39,11 @@ public class RabinService {
 
         var n = p.multiply(q);
         BigInteger b = generator.generateNumber(BigInteger.valueOf(2).pow(Math.toIntExact(keySize)));
+        publicKey.setN(n);
+        publicKey.setB(b);
 
         response.setModulus(n.toString(16));
-        response.setModulus(b.toString(16));
+        response.setB(b.toString(16));
 
         return response;
     }
@@ -72,6 +79,85 @@ public class RabinService {
         return response;
     }
 
+    // (y, c1, c2)
+    public DecryptResponse decrypt(DecryptRequest request) {
+        DecryptResponse response = new DecryptResponse();
+
+        BigInteger y = new BigInteger(request.getCiphertext(), 16);
+        BigInteger c1 = new BigInteger(request.getParity(), 16);
+        BigInteger c2 = new BigInteger(request.getJacobiSymbol(), 16);
+        var b = publicKey.getB();
+        var n = publicKey.getN();
+        var p = privateKey.getP();
+        var q = privateKey.getQ();
+
+        List<BigInteger> sqrt = mathUtil.sqrt(y.add(b.pow(2).multiply(BigInteger.valueOf(4).modInverse(n))), p, q);
+
+        BigInteger result = null;
+        var x1 = getX(sqrt.get(0), b, n);
+        var x2 = getX(sqrt.get(1), b, n);
+        var x3 = getX(sqrt.get(2), b, n);
+        var x4 = getX(sqrt.get(3), b, n);
+
+        var val = b.multiply(BigInteger.valueOf(2).modInverse(n));
+        var temp1 = x1.add(val);
+        var temp2 = x2.add(val);
+        var temp3 = x3.add(val);
+        var temp4 = x4.add(val);
+
+        var x1c1 = temp1.mod(n).mod(BigInteger.TWO).mod(n).mod(BigInteger.TWO);
+        var x2c1 = temp2.mod(n).mod(BigInteger.TWO).mod(n).mod(BigInteger.TWO);
+        var x3c1 = temp3.mod(n).mod(BigInteger.TWO).mod(n).mod(BigInteger.TWO);
+        var x4c1 = temp4.mod(n).mod(BigInteger.TWO).mod(n).mod(BigInteger.TWO);
+
+        var x1c2 = mathUtil.getJacobiSymbol(temp1, n);
+        var x2c2 = mathUtil.getJacobiSymbol(temp2, n);
+        var x3c2 = mathUtil.getJacobiSymbol(temp3, n);
+        var x4c2 = mathUtil.getJacobiSymbol(temp4, n);
+
+        if (c1.equals(x1c1) && c2.equals(x1c2)) {
+            result = x1;
+        }
+        if (c1.equals(x2c1) && c2.equals(x2c2)) {
+            result = x2;
+        }
+        if (c1.equals(x3c1) && c2.equals(x3c2)) {
+            result = x3;
+        }
+        if (c1.equals(x4c1) && c2.equals(x4c2)) {
+            result = x4;
+        }
+
+        if (result == null) {
+            throw new RuntimeException("Cannot be decrypted");
+        }
+
+        response.setMessage(result.toString(16));
+
+        return response;
+    }
+
+    private BigInteger getX(BigInteger x, BigInteger b, BigInteger n) {
+        BigInteger inv2 = BigInteger.valueOf(2).modInverse(n);
+        return x.subtract(b.multiply(inv2)).mod(n);
+    }
+
+    public SignResponse sign(SignRequest request) {
+        SignResponse response = new SignResponse();
+
+
+
+        return response;
+    }
+
+    public VerifyResponse verify(VerifyRequest request) {
+        VerifyResponse response = new VerifyResponse();
+
+
+
+        return response;
+    }
+
     private BigInteger formatMessage(BigInteger message, int n) {
         int l = (int) Math.ceil((double) n / 8);
 
@@ -83,22 +169,4 @@ public class RabinService {
 
         return x;
     }
-
-    // (y, c1, c2)
-    public DecryptResponse decrypt(DecryptRequest request) {
-        DecryptResponse response = new DecryptResponse();
-
-
-
-        return response;
-    }
-
-    public SignResponse sign(SignRequest request) {
-        throw new UnsupportedOperationException();
-    }
-
-    public VerifyResponse verify(VerifyRequest request) {
-        throw new UnsupportedOperationException();
-    }
-
 }
